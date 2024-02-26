62c20e364e4361c5615d6a8dcfba24dd0d3097f5
Richie-gitonga <gitongar00@gmail.com>
2024-02-26 12:58:02 +0300

    created user input python file that prints out a message for user

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your Age: ")) + 1
+location = input("Enter your location: ")
+
+#birthday message
+print(f"hello {name} from {location}. I wish you a happy birthday as you turn {age}")
